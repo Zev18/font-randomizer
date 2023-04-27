e6fb0ba9fb31dd4d1baf2218917ad3d163593323
zev18 <zevross@gmail.com>
2023-04-27 18:58:51 -0400

    Added status bar icons

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { QuickPickItem, window } from "vscode";
+
+interface FontMenuItem extends QuickPickItem {
+  type: "font" | "button";
+}
+
+let fontLabel: vscode.StatusBarItem;
+let fontRefresh: vscode.StatusBarItem;
 
 function randomize() {
   let currentFont = vscode.workspace
@@ -23,14 +31,31 @@ function randomize() {
     .getConfiguration()
     .update("editor.fontFamily", randomFont + ", monospace", true);
   vscode.window.showInformationMessage("Font changed to " + randomFont);
+
+  updateFontLabel(randomFont);
 }
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate({ subscriptions }: vscode.ExtensionContext) {
   const activateOnStartup = vscode.workspace
     .getConfiguration("fontRandomizer")
     .get<boolean>("activateOnStartup");
+
+  fontLabel = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  fontRefresh = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  subscriptions.push(fontLabel, fontRefresh);
+
+  fontRefresh.command = "font-randomizer.randomize";
+  fontRefresh.text = "$(refresh)";
+  fontRefresh.tooltip = "Randomize font";
+  fontRefresh.show();
 
   if (activateOnStartup) {
     randomize();
@@ -41,7 +66,96 @@ export function activate(context: vscode.ExtensionContext) {
     randomize
   );
 
-  context.subscriptions.push(disposable);
+  subscriptions.push(
+    disposable,
+    vscode.commands.registerCommand("font-randomizer.pick-font", () =>
+      pickFont()
+    )
+  );
+}
+
+function updateFontLabel(font: string) {
+  fontLabel.text = font.split(",")[0];
+  fontLabel.tooltip = "Change font";
+  fontLabel.command = "font-randomizer.pick-font";
+  fontLabel.show();
+}
+
+// font picker function
+async function pickFont(): Promise<void> {
+  // get fonts
+  let menuFonts = vscode.workspace
+    .getConfiguration("fontRandomizer")
+    .get<string[]>("fontList") || ["monospace"];
+  // construct menu items
+  const menuItems: FontMenuItem[] = [
+    ...menuFonts.map(
+      (font) =>
+        <FontMenuItem>{
+          label: font,
+          type: "font",
+        }
+    ),
+    {
+      alwaysShow: true,
+      label: "$(debug-configure) Manage fonts...",
+      type: "button",
+    },
+  ];
+
+  let config = vscode.workspace.getConfiguration("editor");
+  let currentFont = config.get("fontFamily") as string;
+
+  // show picker
+  const selection = await window.showQuickPick(menuItems, {
+    placeHolder: "Select Font",
+    onDidSelectItem: (selection: FontMenuItem) => {
+      // show original settings or selected fong
+      config.update(
+        "fontFamily",
+        selection.type === "button" ? config.fontFamily : selection.label,
+        true
+      );
+      updateFontLabel(selection.label);
+    },
+  });
+
+  // in case of cancel
+  if (!selection) {
+    config.update("fontFamily", currentFont, true);
+    updateFontLabel(currentFont);
+  }
+
+  if (selection && selection.type === "button") {
+    // open settings if "manage fonts" is selected
+    openSetting("fontRandomizer.fontList");
+    updateFontLabel(currentFont);
+  }
+}
+
+// helper function to open a certain setting
+function openSetting(setting: string) {
+  const config = vscode.workspace.getConfiguration();
+  const section = setting.includes(".")
+    ? setting.substring(0, setting.lastIndexOf("."))
+    : setting;
+  vscode.commands
+    .executeCommand("workbench.action.openSettings", section)
+    .then(() => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const regex = new RegExp(`"${setting}":`);
+        const line = editor.document
+          .getText()
+          .split("\n")
+          .findIndex((text) => regex.test(text));
+        if (line !== -1) {
+          const position = new vscode.Position(line, 0);
+          editor.selection = new vscode.Selection(position, position);
+          editor.revealRange(new vscode.Range(position, position));
+        }
+      }
+    });
 }
 
 // This method is called when your extension is deactivated
